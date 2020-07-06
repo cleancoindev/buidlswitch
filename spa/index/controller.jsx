de19f0928ad0e6d2897ad3a8bd0a24bfa71240f6
Marco Vasapollo <ceo@metaring.com>
2020-07-06 20:04:42 +0200

    Backend Integration

--- spa/index/controller.jsx
+++ spa/index/controller.jsx
@@ -1,4 +1,20 @@
 var IndexController = function (view) {
     var context = this;
     context.view = view;
+
+    context.loadData = async function loadData() {
+        var length = await window.blockchainCall(window.vasaPowerSwitch.methods.length);
+        var slots = [];
+        var currentSlot = null;
+        var currentBlock = parseInt(await window.web3.eth.getBlockNumber());
+        for(var i = 0; i < length; i++) {
+            var data = await window.blockchainCall(window.vasaPowerSwitch.methods.timeWindow, i);
+            slots.push(data);
+            !currentSlot && currentBlock <= parseInt(data[0]) && (currentSlot = data);
+        }
+        var approved = !window.walletAddress ? false : parseInt(await window.blockchainCall(window.oldToken.methods.allowance, window.walletAddress, window.vasaPowerSwitch.options.address)) > 0;
+        var balanceOf = !window.walletAddress ? '0' : await window.blockchainCall(window.oldToken.methods.balanceOf, window.walletAddress);
+        var totalMintable = await window.blockchainCall(window.vasaPowerSwitch.methods.totalMintable);
+        context.view.setState({slots, currentSlot, currentBlock, approved, balanceOf, totalMintable, newVotingTokenAddress : (await window.web3.eth.dfoHub.votingToken).options.address});
+    };
 };
--- spa/switch/view.jsx
+++ spa/switch/view.jsx
@@ -1,32 +1,66 @@
 var Switch = React.createClass({
-    
     requiredScripts: [
         'spa/loader.jsx',
         'spa/bigLoader.jsx'
     ],
-    
+    max() {
+        this.props.balanceOf && (this.input.value = window.fromDecimals(this.props.balanceOf, 18));
+        this.onChange();
+    },
+    approve(e) {
+        e && e.preventDefault && e.preventDefault(true) && e.stopPropagation && e.stopPropagation(true);
+        if(e.currentTarget.className.indexOf("active") === -1) {
+            return;
+        }
+        this.controller.approve();
+    },
+    switch(e) {
+        e && e.preventDefault && e.preventDefault(true) && e.stopPropagation && e.stopPropagation(true);
+        if(e.currentTarget.className.indexOf("active") === -1) {
+            return;
+        }
+        this.controller.switchOperation(this.input.value);
+    },
+    onChange() {
+        if(!this.input || !this.switchFinal) {
+            return;
+        }
+        this.switchFinal.innerHTML = '0';
+        var value = this.input.value;
+        if(isNaN(parseInt(value))) {
+            return;
+        }
+        if(!this.props.currentSlot) {
+            return;
+        }
+        value = window.toDecimals(value, 18);
+        value = window.web3.utils.toBN(value).mul(window.web3.utils.toBN(this.props.currentSlot[1])).div(window.web3.utils.toBN(this.props.currentSlot[2]));
+        this.switchFinal.innerHTML = window.fromDecimals(value, 18);
+    },
     render() {
+        var _this = this;
         return (<section>
             <section className="switchBox">
             <h3>Switch</h3>
                 <section className="switchTools">
-                    <a className="switchAll">Max</a>
-                    <input type="number"></input>
-                    <a className="switchLink">$BUIDL<b> V1</b></a>
+                    <a href="javascript:;" className="switchAll" onClick={this.max}>Max</a>
+                    <input type="number" ref={ref => (this.input = ref) && (ref.value = window.fromDecimals(this.props.balanceOf, 18)) && this.onChange()} onChange={this.onChange}/>
+                    <a className="switchLink" href={window.getNetworkElement("etherscanURL") + "token/" + window.oldToken.options.address} target="_blank">$BUIDL<b> V1</b></a>
                     <img src="/assets/img/buidl-logo.png"></img>
                 </section>
             <h3>For</h3>
                 <section className="switchTools">
-                    <span className="switchFinal">250</span>
-                    <a className="switchLink">$buidl<b> V2</b></a>
+                    <span ref={ref => (this.switchFinal = ref) && this.onChange()} className="switchFinal">0</span>
+                    <a className="switchLink" href={window.getNetworkElement("etherscanURL") + "token/" + this.props.newVotingTokenAddress} target="_blank">$buidl<b> V2</b></a>
                     <img src="/assets/img/buidlv2-logo.png"></img>
                 </section>
                 <section className="switchActions">
-                    <a className="switchAction active">Approve</a>
-                    <a className="switchAction">Switch</a>
+                    {this.props.currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (!this.props.approved ? " active" : "")} onClick={this.approve}>Approve</a>}
+                    {this.props.currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (this.props.approved ? " active" : "")} onClick={this.switch}>Switch</a>}
+                    {this.props.currentSlot && !window.walletAddress && <a href="javascript:;" onClick={() => window.ethereum.enable().then(() => window.getAddress()).then(() => _this.emit('ethereum/ping'))} className="switchAction active">Connect</a>}
                 </section>
-                <p>Current Switch bonus is <b>X1.5</b> until the block n. </p>
-                <p>Disclamer: Switching $BUIDL V1 to $buidl V2 is an irreversible action, do it at your own risk</p>
+                {this.props.currentSlot && <p>Current Switch bonus is <b>X{window.numberToString(parseInt(this.props.currentSlot[1]) / parseInt(this.props.currentSlot[2]))}</b> until the block n. <a href={window.getNetworkElement("etherscanURL") + "block/" + this.props.currentSlot[0]} target="_blank">{this.props.currentSlot[0]}</a></p>}
+                {this.props.currentSlot && <p>Disclamer: Switching $BUIDL V1 to $buidl V2 is an irreversible action, do it at your own risk</p>}
             </section>
         </section>);
     }
